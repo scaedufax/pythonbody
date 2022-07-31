1ecf3838ec3a57f9e44360196f7ad32aefdc1521
Uli Roth <uli@shad0wfax.de>
2022-07-31 10:03:15 +0200

    improved omp grav_pot calculations

--- ffi/grav_pot.c
+++ ffi/grav_pot.c
@@ -19,20 +19,18 @@ struct thread_args {
 };
 
 double grav_pot_omp(double *m, double *x1, double *x2, double *x3, double *EPOT, int n, int num_threads) {
-	if num_threads == NULL {
-		num_threads = sysconf(_SC_NPROCESSORS_ONLN)
-	}
-
 	#pragma omp parallel for
 	for (int i = 0; i < n; i++) {
+		double EPOT_i = 0.0;
 		for (int j = i+1; j < n; j++) {
 			double dist = sqrt((x1[i] - x1[j])*(x1[i] - x1[j]) + (x2[i] - x2[j])*(x2[i] - x2[j]) + (x3[i] - x3[j])*(x3[i] - x3[j]));
 			double epot_ij = -m[i]*m[j]/dist;
-			#pragma omp atomic
-			EPOT[i] += epot_ij;
+			EPOT_i += epot_ij;
 			#pragma omp atomic
 			EPOT[j] += epot_ij;
 		}
+		#pragma omp atomic
+		EPOT[i] += EPOT_i;
 	}
 }
 
@@ -54,9 +52,6 @@ void *grav_pot_thread(void *args) {
 		for (int j = i+1; j < n; j++) {
 			double dist = sqrt((x1[i] - x1[j])*(x1[i] - x1[j]) + (x2[i] - x2[j])*(x2[i] - x2[j]) + (x3[i] - x3[j])*(x3[i] - x3[j]));
 			double epot_ij = -m[i]*m[j]/dist;
-			//printf("Thread %d, i=%d, j=%d\n", thread_id,i,j);
-			//printf("i: m=%f x1=%f, x2=%f, x3=%f\n", m[i], x1[i], x2[i],x3[i]);
-			//printf("j: m=%f x1=%f, x2=%f, x3=%f\n", m[j], x1[j], x2[j],x3[j]);
 			EPOT[i] += epot_ij;
 			EPOT[j] += epot_ij;
 		}

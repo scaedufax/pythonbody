043b90798d6007166921bce0346ac046f20fd688
Uli Roth <uli@shad0wfax.de>
2022-09-07 10:02:15 +0200

    removed cuda

--- tests.py
+++ tests.py
@@ -20,9 +20,15 @@ if __name__ == "__main__":
         "X2": np.random.rand(N),
         "X3": np.random.rand(N),
         })
-    for c_func in ["ocl", "omp", "unthreaded"]:
-        s = dt.datetime.now()
-        #eval(f"EPOT_{c_func} = None")
-        eval(f"grav_pot(df, c_func='{c_func}')")
-        print(f"{c_func.replace('unthreaded','unthrd')}:\t{(dt.datetime.now() - s).microseconds + 1000000 * (dt.datetime.now() - s).seconds}")
-
+    EPOT = {}
+    EPOT_c_funcs = [ "unthreaded", "omp", "ocl", "cuda"]
+    for c_func in EPOT_c_funcs:
+        try:
+            s = dt.datetime.now()
+            #eval(f"EPOT_{c_func} = None")
+            EPOT[c_func] = eval(f"grav_pot(df, c_func='{c_func}')")
+            print(f"{c_func.replace('unthreaded','unthrd')}:\t{(dt.datetime.now() - s).microseconds/1000000 + (dt.datetime.now() - s).seconds}")
+            if EPOT_c_funcs.index(c_func) != 0:
+              np.testing.assert_allclose(EPOT[EPOT_c_funcs[EPOT_c_funcs.index(c_func) - 1]], EPOT[c_func])
+        except:
+            continue

d7ae22847db3ea80217b8b8396ba539076bce8eb
Uli Roth <uli@shad0wfax.de>
2022-09-23 09:41:09 +0200

    added rel/cm_LZ/Ekin(_spec) to binaries

--- snap/binaries.py
+++ snap/binaries.py
@@ -12,8 +12,37 @@ class Binaries(nbdf):
         """
         see http://web.pd.astro.it/mapelli/colldyn3.pdf
         """
-        self["Eb"] = ((0.5*self["M1"].values*self["M2"].values)/self[["M1","M2"]].sum(axis=1).values * np.linalg.norm(self[["relV1", "relV2", "relV3"]],axis=1))**2 - (G*(self["M1"].values*self["M2"].values)/(np.linalg.norm(self[["relX1", "relX2", "relX3"]],axis=1)))
+        self["Eb"] = ((0.5*self["M1"].values*self["M2"].values)/self[["M1","M2"]].sum(axis=1).values * np.linalg.norm(self[["relV1", "relV2", "relV3"]],axis=1)**2) - (G*(self["M1"].values*self["M2"].values)/(np.linalg.norm(self[["relX1", "relX2", "relX3"]],axis=1)))
         return self["Eb"]
+
+    def calc_relLZ_spec(self):
+        self["relLZ_spec"] = self["relX1"]*self["relV2"] - self["relX2"]*self["relV1"]
+
+    def calc_relLZ(self):
+        if "relLZ_spec" not in self.columns:
+            self.calc_relLZ_spec()
+        self["relLZ"] = self["relLZ_spec"] * (self["M1"].values*self["M2"].values)/self[["M1","M2"]].sum(axis=1).values
+    
+    def calc_cmLZ_spec(self):
+        self["cmLZ_spec"] = self["cmX1"]*self["cmV2"] - self["cmX2"]*self["cmV1"]
+
+    def calc_cmLZ(self):
+        if "cmLZ_spec" not in self.columns:
+            self.calc_relLZ_spec()
+        self["cmLZ"] = self["cmLZ_spec"] * (self["M1"].values*self["M2"].values)/self[["M1","M2"]].sum(axis=1).values
+
+    def calc_relEKIN_spec(self):
+        self["relEKIN_spec"] = 0.5*np.linalg.norm(self.cluster_data[["relV1", "relV2", "relV3"]], axis=1)**2
+    def calc_relEKIN(self):
+        if "relEKIN_spec" not in self.columns:
+            self.calc_relEKIN_spec()
+        self["relEKIN"] = self["relEKIN_spec"] * (self["M1"].values*self["M2"].values)/self[["M1","M2"]].sum(axis=1).values
+    def calc_cmEKIN_spec(self):
+        self["cmEKIN_spec"] = 0.5*np.linalg.norm(self.cluster_data[["cmV1", "cmV2", "cmV3"]], axis=1)**2
+    def calc_relEKIN(self):
+        if "cmEKIN_spec" not in self.columns:
+            self.calc_cmEKIN_spec()
+        self["cmEKIN"] = self["cmEKIN_spec"] * (self["M1"].values*self["M2"].values)/self[["M1","M2"]].sum(axis=1).values
 
     def filter(self,value):
         if value not in self.filters:

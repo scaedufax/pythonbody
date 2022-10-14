9682aa32f73142737628d55193b893d1f0db32a7
Uli Roth <uli@shad0wfax.de>
2022-10-14 10:10:43 +0200

    differntiate between rel and cm Eb_spec

--- snap/binaries.py
+++ snap/binaries.py
@@ -15,11 +15,15 @@ class Binaries(nbdf):
         """"""
         self["Eb"] = ((0.5*self["M1"].values*self["M2"].values)/self[["M1","M2"]].sum(axis=1).values * np.linalg.norm(self[["relV1", "relV2", "relV3"]],axis=1)**2) - (G*(self["M1"].values*self["M2"].values)/(np.linalg.norm(self[["relX1", "relX2", "relX3"]],axis=1)))
         return self["Eb"]"""
-    def calc_Eb_spec(self):
+    def calc_relEb_spec(self):
         if "relEKIN_spec" not in self.columns:
             self.calc_relEKIN_spec()
-        self["Eb_spec"] = self["relPOT"] + self["relEKIN_spec"]
-
+        self["relEb_spec"] = self["relPOT"] + self["relEKIN_spec"]
+    
+    def calc_relEb_spec(self):
+        if "cmEKIN_spec" not in self.columns:
+            self.calc_cmEKIN_spec()
+        self["cmEb_spec"] = self["relPOT"] + self["cmEKIN_spec"]
 
     def calc_relLZ_spec(self):
         self["relLZ_spec"] = self["relX1"]*self["relV2"] - self["relX2"]*self["relV1"]

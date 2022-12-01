24eda66747a1685c3d340492d8f5f86368789cef
Uli Roth <uli@shad0wfax.de>
2022-12-01 11:27:17 +0100

    dat10.py: Added rotation by yaw, pitch and roll, rotation along axis by angle, and rotate such that angular momentum is aligned with z-axis

--- dat10.py
+++ dat10.py
@@ -87,6 +87,10 @@ class dat10():
         return self._data._repr_html_()
 
     @property
+    def loc(self):
+        return self._data.loc
+
+    @property
     def iloc(self):
         return self._data.iloc
     
@@ -111,7 +115,7 @@ class dat10():
     @property
     def EKIN(self):
         if self._EKIN is None:
-            self._data["EKIN"] = 1/2 * self.iloc[:,0]*np.linalg.norm(self.iloc[:,4:7],axis=1)**2
+            self._data["EKIN"] = 1/2 * self.loc[:,"M"]*np.linalg.norm(self.loc[:, ["V1", "V2", "V3"]], axis=1)**2
             self._EKIN = self._data["EKIN"].sum()
         return self._EKIN
     
@@ -125,21 +129,21 @@ class dat10():
 
     @property
     def ZMBAR(self):
-        return self._data.iloc[:,0].mean() 
+        return self._data.loc[:, "M"].mean()
     
     @property
     def RBAR(self):
-        return np.linalg.norm(self.iloc[:,1:4],axis=1).mean()
+        return np.linalg.norm(self.loc[:, ["X1", "X2", "X3"]], axis=1).mean()
     
     @property
     def COM(self):
         if self._com is None:
-            self._com = 1/self.iloc[:,0].sum() * np.sum(self.iloc[:,[1,2,3]].multiply(self.iloc[:,0],axis=0))
+            self._com = 1/self.loc[:, "M"].sum() * np.sum(self.loc[:, ["X1", "X2", "X3"]].multiply(self.loc[:, "M"], axis=0))
         return self._com
     
     @property
     def AVMASS(self):
-        return self._data.iloc[:,0].mean()   
+        return self._data.loc[:, "M"].mean()   
         
     def __setitem__(self, key, item):
         self._data[key] = item
@@ -170,7 +174,99 @@ class dat10():
     def _check_non_empty(self):
         if not self._data:
             self.logger.warning("Data is empty, have you loaded the file?")
-            
+    
+    def rotate(self, yaw: float = 0, pitch: float = 0, roll: float = 0):
+        """
+        Rotates position (and velocities if available)
+
+        Parameters:
+            yaw (float): yaw in radians
+            pitch (float): pitch in radians
+            roll (float): roll in radians
+        """
+
+        # initialize rotation matrices
+        rot_mat_yaw = np.zeros((3, 3))
+        rot_mat_pitch = np.zeros((3, 3))
+        rot_mat_roll = np.zeros((3, 3))
+
+        rot_mat_yaw[0][0] = np.cos(yaw)
+        rot_mat_yaw[0][1] = -np.sin(yaw)
+        rot_mat_yaw[1][0] = np.sin(yaw)
+        rot_mat_yaw[1][1] = np.cos(yaw)
+        rot_mat_yaw[2][2] = 1
+
+        rot_mat_pitch[0][0] = np.cos(pitch)
+        rot_mat_pitch[0][2] = np.sin(pitch)
+        rot_mat_pitch[1][1] = 1
+        rot_mat_pitch[2][0] = -np.sin(pitch)
+        rot_mat_pitch[2][2] = np.cos(pitch)
+
+        rot_mat_roll[0][0] = 1
+        rot_mat_roll[1][1] = np.cos()
+        rot_mat_roll[1][2] = -np.sin(roll)
+        rot_mat_roll[2][1] = np.sin(roll)
+        rot_mat_roll[2][2] = np.cos(roll)
+
+        rot_mat = np.matmul(np.matmul(rot_mat_yaw, rot_mat_pitch), rot_mat_roll)
+
+        self[["V1", "V2", "V3"]] = rot_mat.dot(self[["V1", "V2", "V3"]].T.values).T
+        # rotate positions
+        self[["X1", "X2", "X3"]] = rot_mat.dot(self[["X1", "X2", "X3"]].T.values).T
+        self._L = None
+
+    def rotate_axis(self, axis: np.array, angle: float):
+        """
+        Rotates position (and velocities if available) around a given axis by angle
+
+        Parameters:
+            axis (np.array, list): axis to rotate around, must contain 3 values
+            angle (float): angle to rotate by
+        """
+        if type(axis) == list:
+            axis = np.array(axis)
+        if axis.shape != (3,):
+            raise ValueError(f"Axis must have shape (3,) but is {axis.shape}")
+
+        # initialize rotation matrix
+        rot_mat = np.zeros((3,3))
+
+        # see e.g. https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
+        rot_mat[0][0] = np.cos(angle) + axis[0]**2*(1-np.cos(angle))
+        rot_mat[0][1] = axis[0]*axis[1]*(1-np.cos(angle))-axis[2]*np.sin(angle)
+        rot_mat[0][2] = axis[0]*axis[2]*(1-np.cos(angle))+axis[1]*np.sin(angle)
+        rot_mat[1][0] = axis[1]*axis[0]*(1-np.cos(angle))+axis[2]*np.sin(angle)
+        rot_mat[1][1] = np.cos(angle)+axis[1]**2*(1-np.cos(angle))
+        rot_mat[1][2] = axis[1]*axis[2]*(1-np.cos(angle))-axis[0]*np.sin(angle)
+        rot_mat[2][0] = axis[2]*axis[0]*(1-np.cos(angle))-axis[1]*np.sin(angle)
+        rot_mat[2][1] = axis[2]*axis[1]*(1-np.cos(angle))+axis[0]*np.sin(angle)
+        rot_mat[2][2] = np.cos(angle)+axis[2]**2*(1-np.cos(angle))
+        
+        # rotate velocities
+        self[["V1", "V2", "V3"]] = rot_mat.dot(self[["V1", "V2", "V3"]].T.values).T
+        # rotate positions
+        self[["X1", "X2", "X3"]] = rot_mat.dot(self[["X1", "X2", "X3"]].T.values).T
+        self._L = None
+
+    def rotate_angular_momentum_along_zaxis(self):
+        """
+        Rotates position and velocities such that the angular momentum is
+        aligned with z-axis
+        """
+
+        momentum = self.L
+        # calculate normal between angular momentum and z-axis.
+        normal = np.cross(self.L, (0, 0, 1))
+
+        # normalize angular momentum and normal
+        momentum = momentum/np.linalg.norm(momentum)
+        normal = normal/np.linalg.norm(normal)
+
+        # calculate angle to rotate along normal by
+        angle = np.arccos(momentum.dot((0, 0, 1)))
+
+        self.rotate_axis(normal, angle)
+
     def len(self):
         return self._data.shape[0]
      
@@ -178,7 +274,7 @@ class dat10():
         """
         Changes positions into center of mass system
         """
-        self._data.iloc[:,[1,2,3]] = self._data.iloc[:,[1,2,3]] - self.COM
+        self._data.loc[:, ["X1", "X2", "X3"]] = self._data.loc[:, ["X1", "X2", "X3"]] - self.COM
         self._com = None
     
     def _setup_logger(self):

71daf5c125281241ff9e28c3c3b1c20b56963f8e
Uli Roth <uli@shad0wfax.de>
2023-01-24 11:07:06 +0100

    plots.py: added option to pass a reference run

--- plots.py
+++ plots.py
@@ -8,8 +8,9 @@ from .nbody import nbody
 
 
 def gen_x1_x2_and_x1_x3_plots(run: nbody,
-                              path: str,
-                              scatter_kw_args: dict,
+                              ref_run: nbody = None,
+                              path: str = "./",
+                              scatter_kw_args: dict = {},
                               base_file_name: str = "",
                               image_ext: str = "png",
                               xlim: tuple = None,
@@ -23,6 +24,8 @@ def gen_x1_x2_and_x1_x3_plots(run: nbody,
 
     :param run: nbody project
     :type run: nbody
+    :param ref_run: adds a reference plot below the main one
+    :type ref_run: nbody
     :param path: where to store the the output plots
     :type path: str
     :param scatter_kw_args: arguments to pass to scatter
@@ -39,17 +42,22 @@ def gen_x1_x2_and_x1_x3_plots(run: nbody,
     :param save_neighbour_rho_to_snap: As Neighbour_rho needs to be calculated
         anyway here, this can be used to save them to the snap files!
     :type save_neighbour_rho_to_snap: bool
+    :param force_neighbour_rho_recalc: Force recalculation of neighbour_rho data
+    :type force_neighbour_rho_recalc: bool
+    :param n_procs: number of processes to use during multiprocessing
+    :type n_procs: int:
     """
-
-    print(run.snap.snap_data.index.values)
-
+    
+    # get number of processors if nothing was passend
     n_procs = mp.cpu_count() if n_procs is None else n_procs
 
     with mp.Manager() as manager:
+        # Initialise lock and function
         lock = manager.Lock()
         func = partial(_gen_x1_x2_and_x1_x3_plot,
                        lock=lock,
                        run=run,
+                       ref_run=ref_run,
                        path=path,
                        scatter_kw_args=scatter_kw_args,
                        base_file_name=base_file_name,
@@ -58,10 +66,16 @@ def gen_x1_x2_and_x1_x3_plots(run: nbody,
                        ylim=ylim,
                        save_neighbour_rho_to_snap=save_neighbour_rho_to_snap,
                        force_neighbour_rho_recalc=force_neighbour_rho_recalc)
+        # start pool
         with mp.Pool(processes=n_procs) as pool:
-            _total = run.snap.snap_data.index.values.shape[0]
+            # make sure to have nice tqdm output
+            max_time = run.snap.snap_data.index.values.max()
+            if ref_run is not None:
+                max_time = min(max_time, ref_run.snap.snap_data.index.values.max())
+            mask = run.snap.snap_data.index.values <= max_time
+            _total = mask.sum()
             with tqdm(total=_total) as pbar:
-                for _ in pool.imap_unordered(func, run.snap.snap_data.index.values):
+                for _ in pool.imap_unordered(func, run.snap.snap_data.index.values[mask]):
                     pbar.update()
 
 
@@ -83,6 +97,7 @@ def _gen_x1_x2_and_x1_x3_plot(time: float,
                               lock: mp.Lock,
                               path: str,
                               scatter_kw_args: dict,
+                              ref_run: nbody = None,
                               base_file_name: str = "",
                               image_ext: str = "png",
                               xlim: tuple = None,
@@ -90,7 +105,11 @@ def _gen_x1_x2_and_x1_x3_plot(time: float,
                               save_neighbour_rho_to_snap: bool = True,
                               force_neighbour_rho_recalc: bool = False,
                               ):
+
+    # load time step (also in ref cluster!)
     run.snap.load_cluster(time)
+    if ref_run is not None:
+        ref_run.snap.load_cluster(time)
 
     if (("NEIGHBOUR_RHO_M" not in run.snap.cluster_data.columns)
         or ("NEIGHBOUR_RHO_N" not in run.snap.cluster_data.columns)
@@ -100,10 +119,15 @@ def _gen_x1_x2_and_x1_x3_plot(time: float,
             lock.acquire()
             try:
                 run.snap.cluster_data.calc_NEIGHBOUR_RHO()
+                if ref_run is not None:
+                    ref_run.snap.cluster_data.calc_NEIGHBOUR_RHO()
 
                 if save_neighbour_rho_to_snap:
                     run.snap.save_cols({"NEIGHBOUR_RHO_N": "PNB_CD_NEIGHBOUR_RHO_N",
                                         "NEIGHBOUR_RHO_M": "PNB_CD_NEIGHBOUR_RHO_M"})
+                    if ref_run is not None:
+                        ref_run.snap.save_cols({"NEIGHBOUR_RHO_N": "PNB_CD_NEIGHBOUR_RHO_N",
+                                                "NEIGHBOUR_RHO_M": "PNB_CD_NEIGHBOUR_RHO_M"})
             finally:
                 lock.release()
     
@@ -111,11 +135,16 @@ def _gen_x1_x2_and_x1_x3_plot(time: float,
                                       ignore_index=True,
                                       inplace=True,
                                       ascending=True)
+    if ref_run is not None:
+        ref_run.snap.cluster_data.sort_values("NEIGHBOUR_RHO_M",
+                                              ignore_index=True,
+                                              inplace=True,
+                                              ascending=True)
 
-    fig, axes = plt.subplots(nrows=1,
+    fig, axes = plt.subplots(nrows=1 if ref_run is None else 2,
                              ncols=2,
-                             figsize=(6.4*2, 6.4))
-    
+                             figsize=(6.4*2, 6.4 if ref_run is None else 6.4*2))
+    axes = axes.flatten()
     axes[0].scatter(run.snap.cluster_data["X1"],
                     run.snap.cluster_data["X2"],
                     c=np.log10(run.snap.cluster_data["NEIGHBOUR_RHO_M"]),
@@ -135,6 +164,30 @@ def _gen_x1_x2_and_x1_x3_plot(time: float,
     if ylim is not None:
         axes[0].set_ylim(*ylim)
         axes[1].set_ylim(*ylim)
+
+    if ref_run is not None:
+        axes[2].scatter(ref_run.snap.cluster_data["X1"],
+                        ref_run.snap.cluster_data["X2"],
+                        c=np.log10(ref_run.snap.cluster_data["NEIGHBOUR_RHO_M"]),
+                        **scatter_kw_args)
+        axes[3].scatter(ref_run.snap.cluster_data["X1"],
+                        ref_run.snap.cluster_data["X3"],
+                        c=np.log10(ref_run.snap.cluster_data["NEIGHBOUR_RHO_M"]),
+                        **scatter_kw_args)
+
+        axes[2].set_xlabel("X1")
+        axes[2].set_ylabel("X2")
+        axes[3].set_xlabel("X1")
+        axes[3].set_ylabel("X3")
+        if xlim is not None:
+            axes[2].set_xlim(*xlim)
+            axes[3].set_xlim(*xlim)
+        if ylim is not None:
+            axes[2].set_ylim(*ylim)
+            axes[3].set_ylim(*ylim)
+
+    for ax in axes:
+        ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
 
     fig.suptitle(f"Time = {time}")
     fig.tight_layout()
